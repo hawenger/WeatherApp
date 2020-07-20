1c191334f30a80b1f83bf524c620ca25450ffef4
Ameco <hamecow@gmail.com>
2020-07-20 14:27:11 -0700

    Add javascript api calls

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,46 @@
-$('#select-city').on('click', function(event) {
-    event.preventDefault();
-    let city = $('city-input').val();
-    let queryURL = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={60.99}&lon={30}&dt={1586468027}&appid={19bb59ca9e9c53523fec04a6898c8443}";
+const api= {
+    key: '63183c4ac7d682e53892a0100ab56cb4',
+    baseurl: 'https://api.openweathermap.org/data/2.5/'
+}
 
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response) {
-        $('#weather-div').text(JSON.stringify(response));
-    });
-})
+const searchbox = document.querySelector('.city-input');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(event) {
+    if (event.keyCode == 13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+        return weather.JSON();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date ();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}`
+}
+
+function dateBuilder (d) {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    let day = days[d.getDay()];
+    let date =  d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${year} ${month}`;
+}
